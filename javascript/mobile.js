a6c9fa1526ca6b0baf8bab920f8dd6b8a4e4e9a0
barrosmateusmacedo@gmail.com <59882435+barrosmateus@users.noreply.github.com>
2022-05-03 14:49:36 -0300

    responsividade

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,23 +1,17 @@
-
 const header = document.querySelector("header");
-const footer = document.querySelector("footer");
-const main = document.querySelector("main");
-const nav_button = document.querySelector(".nav-button");
-const nav_menu = document.querySelector(".nav-menu");
+
 
 window.addEventListener('load', () => {
     
     if(window.screen.width <= 800){
        
         header.classList.add("mobile");
-        footer.classList.add("mobile");
-        main.classList.add("mobile");
+       
  
      } else{
 
         header.classList.remove("mobile");
-        footer.classList.remove("mobile"); 
-        main.classList.remove("mobile");
+        
      }
 });
 
@@ -25,19 +19,17 @@ window.addEventListener('resize', () => {
     if(window.screen.width <= 800){
        
         header.classList.add("mobile");
-        footer.classList.add("mobile");
-        main.classList.add("mobile");
+       
  
      } else{
 
         header.classList.remove("mobile");
-        footer.classList.remove("mobile"); 
-        main.classList.remove("mobile");
+        
      }
 
 })
 
-
+/*
 nav_button.addEventListener("click", ()=>{
 
     if(nav_menu.classList.contains("desactive")){
@@ -50,4 +42,4 @@ nav_button.addEventListener("click", ()=>{
         
     }
 
-})
+})*/
