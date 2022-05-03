653367e7cad58689ea7c5b7f0ec2e8900a733c21
barrosmateusmacedo@gmail.com <59882435+barrosmateus@users.noreply.github.com>
2022-05-03 17:19:25 -0300

    responsive

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,21 +1,22 @@
 const container = document.querySelector(".container");
 const header = document.querySelector("header");
-
+const nav_menu = document.querySelector(".nav-menu");
+const nav_button = document.querySelector(".nav-button");
+const footer = document.querySelector("footer");
 
 window.addEventListener('load', () => {
     container.classList.add("mobile");
     if(window.innerWidth <= 800){
 
         container.classList.add("mobile");
-       
         header.classList.add("mobile");
-       
+        footer.classList.add("mobile");
  
      } else{
 
         container.classList.remove("mobile");
-
         header.classList.remove("mobile");
+        footer.classList.remove("mobile");
         
      }
 });
@@ -29,28 +30,31 @@ window.addEventListener('resize', () => {
 
         container.classList.add("mobile");       
         header.classList.add("mobile");
+        footer.classList.add("mobile");
+
        
  
      } else{
 
         container.classList.remove("mobile");
         header.classList.remove("mobile");
+        footer.classList.remove("mobile");
         
      }
 
 })
 
-/*
+
 nav_button.addEventListener("click", ()=>{
 
     if(nav_menu.classList.contains("desactive")){
         nav_menu.classList.remove("desactive");
-        nav_button.innerHTML = '<span class="material-icons">close</span>';
+        nav_button.innerHTML = '<span class="material-symbols-outlined">close</span>';
         
     }else{
         nav_menu.classList.add("desactive");
-        nav_button.innerHTML = '<span class="material-icons">menu</span>';
+        nav_button.innerHTML = '<span class="material-symbols-outlined">menu</span>';
         
     }
 
-})*/
+})
