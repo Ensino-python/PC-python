b62341cd387f84abe306d0155dc77f6867ba6548
barrosmateusmacedo@gmail.com <59882435+barrosmateus@users.noreply.github.com>
2022-05-11 15:48:46 -0300

    tutores images

--- javascript/create-html.js
+++ javascript/create-html.js
@@ -4,42 +4,42 @@ const pessoas = [
 	{
 		"name": "Blenda Gabrielly Souza Morais",
 		"email": "blenda.gabrielly@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K9311917P7",
+		"image_path": "images/profiles/blenda.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/0935318257015270"
 	},
 	{
 		"name": "Davi Ribeiro Sampaio",
 		"email": "davi.ribeiro@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8644073H2",
+		"image_path": "images/profiles/davi.png",
 		"Formação": "Licenciando em Quimica - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/6088355010556031"
 	},
 	{
 		"name": "Filipe Jesus Portilho",
 		"email": "filipe.portilho@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2896710A9",
+		"image_path": "images/profiles/filipe.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/4882730468480550"
 	},
 	{
 		"name": "Gabriel Faria Carmo Oliveira",
 		"email": "gabriel.faria@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8041831Y9",
+		"image_path": "images/profiles/gabriel.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": " http://lattes.cnpq.br/2815580279162008"
 	},
 	{
 		"name": "Guilherme Carvalho Franco",
 		"email": "guilherme.carvalho@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2871377U8",
+		"image_path": "images/profiles/guilherme.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/0747615806741497"
 	},
 	{
 		"name": "Jheovana Hayssa Ferreira Santos",
 		"email": "jheovana.ferreira@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K9534846E0",
+		"image_path": "images/profiles/jheovana.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/4451837998239178"
 	},
@@ -54,7 +54,7 @@ const pessoas = [
 	{
 		"name": "Murillo Santos de Castro",
 		"email": "murillo.castro@estudante.ifgoiano.edu.br",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K9534950A2",
+		"image_path": "images/profiles/murillo.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/0084418328372648"
 	},
@@ -62,14 +62,14 @@ const pessoas = [
 		"name": "Newarney Torrezao da Costa",
 		"email": "newarney.costa@ifgoiano.edu.br",
 		"instagram": "https://www.instagram.com/mateusbmm/",
-		"image_path": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4416169D5",
+		"image_path": "images/profiles/newarney.png",
 		"Formação": "Doutorando em Ciência da Computação - UFU",
 		"lattes_url": "http://lattes.cnpq.br/4986407746430136"
 	},
 	{
 		"name": "Pedro Raphael Inácio Gomes",
 		"email": "pedro.gomes@estudante.ifgoiano.edu.br",
-		"image_path": "images/user.svg",
+		"image_path": "images/profiles/pedro.png",
 		"Formação": "Bacharelando em Ciência da Computação - IF Goiano - Campus Iporá",
 		"lattes_url": "http://lattes.cnpq.br/2047017170310483"
 	}
@@ -79,8 +79,9 @@ pessoas.forEach((pessoa) => {
 
 	integrantes.innerHTML += `		
 	<div class="card-professor">
-		<img class="image-professor" src="${pessoa.image_path}">
-
+		<div class="foto">
+			<img class="image-professor" src="${pessoa.image_path}">
+		</div>
 		<div>
 			<a href="${pessoa.lattes_url}" class="name-professor" target="_blank">${pessoa.name}</a>
 		</div>
